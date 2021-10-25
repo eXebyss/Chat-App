0479ba35b570ef3d886acae8de72433da30efeaf
eXebyss <mysticlord27@gmail.com>
2021-10-26 00:23:54 +0300

    message stylings

--- src/utils/MessageCard/MessageCard.jsx
+++ src/utils/MessageCard/MessageCard.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import './MessageCard.css'
 
 function MessageCard(props) {
 	return (
-		<li className='message-card list-group-item'>
-			<div className='ms-2 bg-info'>
-				<span style={{ color: 'green', fontWeight: 'bold', fontSize: 'large' }}>
-					{props.data.nickname}:
-				</span>
-				<div className='fw-bold'>{props.data.content}</div>
-			</div>
-		</li>
+		<div className='message-container'>
+			<h5 className='message-nickname'>{props.data.nickname}:</h5>
+			{/* <img src='./avatar.jpg' /> */}
+			<p className='message-content'>{props.data.content}</p>
+			<span className='time-right'>{props.data.date}</span>
+		</div>
 	)
 }
 
