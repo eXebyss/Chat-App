96a8df3b4e5ae5b57ad9afecb8f6775bba34bc96
eXebyss <mysticlord27@gmail.com>
2021-11-02 15:22:06 +0200

    MessageCard location changed

--- src/components/messageList/MessageList.jsx
+++ src/components/messageList/MessageList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import MessageCard from '../../utils/MessageCard/MessageCard'
+import MessageCard from '../MessageCard/MessageCard'
 import './MessageList.css'
 
 function MessageList(props) {
